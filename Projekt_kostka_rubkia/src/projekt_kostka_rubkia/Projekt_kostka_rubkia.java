5a2e98c74252c0ea85b12be498be3e6a11022d13
Zolt41 <39956481+Zolt41@users.noreply.github.com>
2019-06-04 22:42:31 +0200

    Add files via upload
    Ejjj Gabriel weź coś z tym zrób bo mnie już głowa od tego boli i nie mam pomysłu

--- Projekt_kostka_rubkia/src/projekt_kostka_rubkia/Projekt_kostka_rubkia.java
+++ Projekt_kostka_rubkia/src/projekt_kostka_rubkia/Projekt_kostka_rubkia.java
@@ -17,6 +17,9 @@ import com.sun.j3d.utils.image.TextureLoader;
 import com.sun.j3d.utils.pickfast.PickCanvas;
 import com.sun.j3d.utils.universe.*;
 import static java.lang.Math.sqrt;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class kostka_rubkia extends Applet implements KeyListener {
 	 
@@ -25,7 +28,9 @@ class kostka_rubkia extends Applet implements KeyListener {
         static SimpleUniverse universe = new SimpleUniverse(canvas);
     
         TransformGroup Transformacja_scianaLewa;
-        TransformGroup Transformacja_scianaSrodkowa;
+        TransformGroup Transformacja_scianaSrodkowaOdOsiX;
+        TransformGroup Transformacja_scianaSrodkowaOdOsiY;
+        TransformGroup Transformacja_scianaSrodkowaOdOsiZ;
         TransformGroup Transformacja_scianaPrawa;
         TransformGroup Transformacja_scianaPrzod;
         TransformGroup Transformacja_scianaTyl;
@@ -61,21 +66,41 @@ class kostka_rubkia extends Applet implements KeyListener {
                 addLights(group);
                
                 
-                Transformacja_scianaSrodkowa = new TransformGroup();
-                Transformacja_scianaSrodkowa.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+                Transformacja_scianaSrodkowaOdOsiX = new TransformGroup();
+                Transformacja_scianaSrodkowaOdOsiX.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
                 Transformacja_scianaPrawa= new TransformGroup();
                 Transformacja_scianaPrawa.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
                 Transformacja_scianaLewa = new TransformGroup();
                 Transformacja_scianaLewa.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+                Transformacja_scianaSrodkowaOdOsiY = new TransformGroup();
+                Transformacja_scianaSrodkowaOdOsiY.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+                Transformacja_scianaGora= new TransformGroup();
+                Transformacja_scianaGora.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+                Transformacja_scianaDol = new TransformGroup();
+                Transformacja_scianaDol.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+                Transformacja_scianaSrodkowaOdOsiZ = new TransformGroup();
+                Transformacja_scianaSrodkowaOdOsiZ.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+                Transformacja_scianaPrzod= new TransformGroup();
+                Transformacja_scianaPrzod.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+                Transformacja_scianaTyl = new TransformGroup();
+                Transformacja_scianaTyl.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
                 
-                MakeCube();
+                MakeCube1();
                 
-                group.addChild(Transformacja_scianaSrodkowa);
-                
+                Transformacja_scianaSrodkowaOdOsiX.addChild(Transformacja_scianaSrodkowaOdOsiY);
+                Transformacja_scianaSrodkowaOdOsiY.addChild(Transformacja_scianaSrodkowaOdOsiZ);
+                Transformacja_scianaPrawa.addChild(Transformacja_scianaGora);
+                Transformacja_scianaGora.addChild(Transformacja_scianaPrzod);
+                Transformacja_scianaLewa.addChild(Transformacja_scianaDol);
+                Transformacja_scianaDol.addChild(Transformacja_scianaTyl);
+                boxTransformGroup.removeChild(box);
+                group.addChild(Transformacja_scianaSrodkowaOdOsiX);
                 group.addChild(Transformacja_scianaPrawa);
                 group.addChild(Transformacja_scianaLewa);
                 universe.addBranchGraph(group);
 		
+                
+                
                 pickCanvas = new PickCanvas(canvas, group);
 		
 		pickCanvas.setMode(PickInfo.PICK_BOUNDS);
@@ -83,21 +108,26 @@ class kostka_rubkia extends Applet implements KeyListener {
                 
                    
 	}
-        public void MakeCube(){
+        public void MakeCube1(){
+            int xpos=0,ypos=0,zpos=0;
             for(float x=0;x<3;x++)
                 {
                     for(float y=0;y<3;y++)
                     {
                         for(float z=0;z<3;z++)
                         {
-                             getScene(x-1,y-1,z-1);
+                             getScene1(x-1,y-1,z-1);
+                             BoxThatWillBeUsed[xpos][ypos][zpos] = box;
+                             zpos++;
                         }
+                        ypos++;
+                        zpos=0; 
                     }
-                }
-            
-            
+                    xpos++;
+                    ypos=0;
+                }  
         }
-        
+       
         public void positionViewer() {
 		ViewingPlatform vp = universe.getViewingPlatform();
                 
@@ -110,7 +140,7 @@ class kostka_rubkia extends Applet implements KeyListener {
 		vp.getViewPlatformTransform().setTransform(t3d);
                 vp.setViewPlatformBehavior(orbit);
 	}
-	public void getScene(float xpos, float ypos, float zpos) {
+	public void getScene1(float xpos, float ypos, float zpos) {
 		
 		box = new Box(.498f, .498f, .498f, Primitive.GENERATE_TEXTURE_COORDS,getAppearance(new Color3f(Color.red)));		 
 		
@@ -120,7 +150,7 @@ class kostka_rubkia extends Applet implements KeyListener {
 		box.getShape(Box.RIGHT).setAppearance(getAppearance(Color.RED));
 		box.getShape(Box.LEFT).setAppearance(getAppearance(Color.GREEN)); 
 		box.getShape(Box.BACK).setAppearance(getAppearance(new Color3f(Color.yellow)));
-                
+              
 		Transform3D transform = new Transform3D();
                 Vector3f MadeRubix = new Vector3f(xpos, ypos, zpos);
                 transform.setTranslation(MadeRubix);
@@ -134,28 +164,25 @@ class kostka_rubkia extends Applet implements KeyListener {
                 boxTransformGroup2 = new TransformGroup();
                 boxTransformGroup2.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
                 
-           if(xpos == 0.0){
-              boxTransformGroup1.addChild(box);
+           if(zpos == 0.0){
+               boxTransformGroup1.addChild(box);
                boxTransformGroup1.setTransform(transform);
-               Transformacja_scianaSrodkowa.addChild(boxTransformGroup1);
+               Transformacja_scianaSrodkowaOdOsiZ.addChild(boxTransformGroup1);
            }
-           else if(xpos == 1.0){
+           else if(zpos == 1.0){
                boxTransformGroup.addChild(box);
                boxTransformGroup.setTransform(transform);
-               Transformacja_scianaPrawa.addChild(boxTransformGroup);
-           }
+               Transformacja_scianaPrzod.addChild(boxTransformGroup);
+            }
            else {
-               
                boxTransformGroup2.addChild(box);
                boxTransformGroup2.setTransform(transform);
-               Transformacja_scianaLewa.addChild(boxTransformGroup2);
+               Transformacja_scianaTyl.addChild(boxTransformGroup2); 
               
-            }
-          
-           
+            }  
 	}
-	
-	public static void addLights(BranchGroup group) {
+       
+		public static void addLights(BranchGroup group) {
 		Color3f light1Color = new Color3f(0.7f, 0.8f, 0.8f);
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0),100.0);
 		Vector3f light1Direction = new Vector3f(4.0f, -7.0f, -12.0f);
@@ -175,6 +202,57 @@ class kostka_rubkia extends Applet implements KeyListener {
 		ap.setColoringAttributes(ca);
 		return ap;
 	}
+        
+        public void obrocwX(TransformGroup group,double katObrotu){
+            Transform3D t3d = new Transform3D();
+            t3d.set(new Vector3f(0.0f,0f,10.0f));
+            obrot1.rotX(katObrotu);
+            group.getTransform(t3d);
+            t3d.get(matrix);
+            t3d.setTranslation(new Vector3d(0.0,0.0,0.0));
+            t3d.mul(obrot1);
+            t3d.setTranslation(new Vector3d(matrix.m03,matrix.m13,matrix.m23));
+            group.setTransform(t3d);
+              try {
+                  TimeUnit.NANOSECONDS.sleep(1);
+              } catch (InterruptedException ex) {
+                  Logger.getLogger(kostka_rubkia.class.getName()).log(Level.SEVERE, null, ex);
+              }
+        }
+        
+        public void obrocwY(TransformGroup group,double katObrotu){
+            Transform3D t3d = new Transform3D();
+            t3d.set(new Vector3f(0.0f,0f,10.0f));
+            obrot1.rotY(katObrotu);
+            group.getTransform(t3d);
+            t3d.get(matrix);
+            t3d.setTranslation(new Vector3d(0.0,0.0,0.0));
+            t3d.mul(obrot1);
+            t3d.setTranslation(new Vector3d(matrix.m03,matrix.m13,matrix.m23));
+            group.setTransform(t3d);
+              try {
+                  TimeUnit.NANOSECONDS.sleep(1);
+              } catch (InterruptedException ex) {
+                  Logger.getLogger(kostka_rubkia.class.getName()).log(Level.SEVERE, null, ex);
+              }
+        }
+        public void obrocwZ(TransformGroup group,double katObrotu){
+            Transform3D t3d = new Transform3D();
+            t3d.set(new Vector3f(0.0f,0f,10.0f));
+            obrot1.rotZ(katObrotu);
+            group.getTransform(t3d);
+            t3d.get(matrix);
+            t3d.setTranslation(new Vector3d(0.0,0.0,0.0));
+            t3d.mul(obrot1);
+            t3d.setTranslation(new Vector3d(matrix.m03,matrix.m13,matrix.m23));
+            group.setTransform(t3d);
+              try {
+                  TimeUnit.NANOSECONDS.sleep(1);
+              } catch (InterruptedException ex) {
+                  Logger.getLogger(kostka_rubkia.class.getName()).log(Level.SEVERE, null, ex);
+              }
+        }
+        
 	
     @Override
     public void keyTyped(KeyEvent e) {
@@ -184,56 +262,76 @@ class kostka_rubkia extends Applet implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_E){
-          MakeCube();
-          Transform3D t3d = new Transform3D();
-          t3d.set(new Vector3f(0.0f,0f,10.0f));
-          obrot1.rotX(Math.PI/2);
-          Transformacja_scianaPrawa.getTransform(t3d);
-          t3d.get(matrix);
-          t3d.setTranslation(new Vector3d(0.0,0.0,0.0));
-          t3d.mul(obrot1);
-          t3d.setTranslation(new Vector3d(matrix.m03,matrix.m13,matrix.m23));
-          Transformacja_scianaPrawa.setTransform(t3d);
-          
+          for(int i=0;i<500;i++)
+          {
+            obrocwX(Transformacja_scianaPrawa,Math.PI/1000);
+          }
         }
-         if(e.getKeyCode() == KeyEvent.VK_R){
-             MakeCube();
-          Transform3D t3d = new Transform3D();
-          t3d.set(new Vector3f(0.0f,0f,10.0f));
-          obrot1.rotX(-Math.PI/2);
-          Transformacja_scianaPrawa.getTransform(t3d);
-          t3d.get(matrix);
-          t3d.setTranslation(new Vector3d(0.0,0.0,0.0));
-          t3d.mul(obrot1);
-          t3d.setTranslation(new Vector3d(matrix.m03,matrix.m13,matrix.m23));
-          Transformacja_scianaPrawa.setTransform(t3d);
+        else if(e.getKeyCode() == KeyEvent.VK_R){
+           for(int i=0;i<500;i++)
+          {
+               obrocwX(Transformacja_scianaPrawa,-Math.PI/1000);
+          }
         }
-        if(e.getKeyCode() == KeyEvent.VK_Q){
-            MakeCube();
-         Transform3D t3d = new Transform3D();
-          t3d.set(new Vector3f(0.0f,0f,10.0f));
-          obrot1.rotX(Math.PI/2);
-          Transformacja_scianaLewa.getTransform(t3d);
-          t3d.get(matrix);
-          t3d.setTranslation(new Vector3d(0.0,0.0,0.0));
-          t3d.mul(obrot1);
-          t3d.setTranslation(new Vector3d(matrix.m03,matrix.m13,matrix.m23));
-          Transformacja_scianaLewa.setTransform(t3d);
-         
+        else if(e.getKeyCode() == KeyEvent.VK_Q){
+          for(int i=0;i<500;i++)
+          {  
+               obrocwX(Transformacja_scianaLewa,Math.PI/1000);
+          }
         }
-         if(e.getKeyCode() == KeyEvent.VK_W){
-           Transformacja_scianaLewa.removeAllChildren();
-           MakeCube();
-         Transform3D t3d = new Transform3D();
-          t3d.set(new Vector3f(0.0f,0f,10.0f));
-          obrot1.rotX(-Math.PI/2);
-          Transformacja_scianaLewa.getTransform(t3d);
-          t3d.get(matrix);
-          t3d.setTranslation(new Vector3d(0.0,0.0,0.0));
-          t3d.mul(obrot1);
-          t3d.setTranslation(new Vector3d(matrix.m03,matrix.m13,matrix.m23));
-          Transformacja_scianaLewa.setTransform(t3d);
-         
+        else if(e.getKeyCode() == KeyEvent.VK_W){
+          for(int i=0;i<500;i++)
+          {  
+                 obrocwX(Transformacja_scianaLewa,-Math.PI/1000);
+          }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_A){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwY(Transformacja_scianaGora,Math.PI/1000);
+            }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_S){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwY(Transformacja_scianaGora,-Math.PI/1000);
+            }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_D){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwY(Transformacja_scianaDol,Math.PI/1000);
+            }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_F){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwY(Transformacja_scianaDol,-Math.PI/1000);
+            }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_Z){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwZ(Transformacja_scianaPrzod,Math.PI/1000);
+            }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_X){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwZ(Transformacja_scianaPrzod,-Math.PI/1000);
+            }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_C){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwZ(Transformacja_scianaTyl,Math.PI/1000);
+            }
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_V){
+            for(int i=0;i<500;i++)
+            {  
+                   obrocwZ(Transformacja_scianaTyl,-Math.PI/1000);
+            }
         }
     }
 
